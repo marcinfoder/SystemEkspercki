eac3ca8a4cc1bffa6eb08cebcbc26d87e34218ec
marcin <200787@student.pwr.edu.pl>
2015-10-31 16:45:58 +0100

    Nunit

---
+++ SystemEksperckiTests/FirstTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+
+namespace SystemEksperckiTests
+{
+    [TestFixture]
+    public class FirstTest
+    {
+        [Test]
+        public void AssertOneIsEqualToOne()
+        {
+            Assert.That(1, Is.EqualTo(1));
+        }
+    }
+}
